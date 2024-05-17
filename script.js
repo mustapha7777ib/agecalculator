237726e871f72fc45e8635707497669f6918e2b7
Mustapha Ibrahim <ibrahimmustapha12345678@gmail.com>
2024-05-17 11:44:21 +0100

    fourth commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var date1 = currentDate();
 function calculateAge(birthDate) {
     var currentDate = new Date();
     var years = currentDate.getFullYear() - birthDate.getFullYear();
@@ -36,20 +36,79 @@ $("#image").on("click", function() {
     else{
         for(i=0; i<thirtyDays.length; i++) {
             if (thirtyDays[i] === monthValue && dayValue >30){
-                    invalidinput();
-                    $(".span1").text("--");
-                    $(".span2").text("--");
-                    $(".span3").text("--"); 
-                    q = false;
-                    break;
-                }
+                $("#e").css("visibility","hidden");
+                $("#f").css("visibility","hidden");
+                invaliddate();
+                $(".span1").text("--");
+                $(".span2").text("--");
+                $(".span3").text("--"); 
+                q = false;
+                break;
             }
+        }
+        if (yearValue > 2024 || yearValue<0) {
+            invalidyear();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if(monthValue > 12 || monthValue < 1){
+            invalidmonth();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if(dayValue > 31 || dayValue < 1){
+            invaliddate();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if(monthValue === 2 && dayValue > 29){
+            invaliddate();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if(monthValue === 2 && dayValue === 29 && yearValue % 4!== 0){
+            invaliddate();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if (date1.mm == monthValue && date1.dd < dayValue && yearValue >= date1.yyyy){
+            invalidyear();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if(date1.mm < monthValue && date1.yyyy <= yearValue){
+            invalidyear();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        if(date1.yyyy < yearValue){
+            invalidyear();
+            $(".span1").text("--");
+            $(".span2").text("--");
+            $(".span3").text("--"); 
+            q = false;
+        }
+        
         };
     if(q){
         $(".span1").text(age.years);
         $(".span2").text(age.months);
         $(".span3").text(age.days); 
-        $(".flex-item3 *").css("display","none")
+        $(".flex-item2 p").css("visibility","hidden")
         $("#a").css('color',"hsl(0, 1%, 44%)");
         $("#b").css('color',"hsl(0, 1%, 44%)");
         $("#c").css('color',"hsl(0, 1%, 44%)");
@@ -59,6 +118,7 @@ $("#image").on("click", function() {
         $("#day").css("border-radius", "2px");
         $("#month").css("border-radius", "2px");
         $("#year").css("border-radius", "2px");
+        currentDate();
 
 
     }
@@ -70,18 +130,44 @@ function errorempty(){
     $("#a").css('color',"hsl(0, 100%, 67%)");
     $("#b").css('color',"hsl(0, 100%, 67%)");
     $("#c").css('color',"hsl(0, 100%, 67%)");
-    $("#day").css("border", "solid red");
+    $("#day").css("border", "1px solid red");
     $("#day").css("border-radius", "2px");
-    $("#month").css("border", "solid red");
+    $("#month").css("border", "1px solid red");
     $("#month").css("border-radius", "2px");
-    $("#year").css("border", "solid red");
+    $("#year").css("border", "1px solid red");
     $("#year").css("border-radius", "2px");
-    $(".flex-item3 *").css("display","flex")
+    $(".flex-item2 p").css("visibility","visible")
 }
 function invaliddate(){
-    $("e").css("display","none");
-    $("f").css("display","none");
+    $("#a").css('color',"hsl(0, 100%, 67%)");
+    $("#day").css("border", "1px solid red");
+    $("#day").css("border-radius", "2px");
     $("#d").text("Must be a valid date");
-    $("#d").css("display","flex");
+    $("#d").css("visibility","visible");
 }
+function invalidyear(){
+    $("#c").css('color',"hsl(0, 100%, 67%)");
+    $("#year").css("border", "1px solid red");
+    $("#year").css("border-radius", "2px");
+    $("#f").text("Must be in the past ");
+    $("#f").css("visibility","visible");
+}
+function invalidmonth(){
+    $("#b").css('color',"hsl(0, 100%, 67%)");
+    $("#month").css("border", "1px solid red");
+    $("#month").css("border-radius", "2px");
+    $("#e").text("Must be a valid month");
+    $("#e").css("visibility","visible");
+}
+function currentDate(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    dd = parseInt(dd);
+    mm = parseInt(mm);
+    yyyy = parseInt(yyyy);
+    return { dd, mm, yyyy };
+}
+
 
